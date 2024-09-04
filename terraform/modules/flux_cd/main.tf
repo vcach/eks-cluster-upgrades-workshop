42f578da3e08cd0b6d2f6c88fe6aec62d30a29dd
Camilo Cortes H <cortes.camilo@outlook.com>
2024-09-03 21:06:59 -0600

    Update main.tf

--- terraform/modules/flux_cd/main.tf
+++ terraform/modules/flux_cd/main.tf
@@ -21,7 +21,7 @@ resource "kubernetes_namespace" "flux_system" {
 resource "helm_release" "flux2-sync" {
   name       = "flux-system"
   namespace   = var.namespace
-  repository = "https://github.com/fluxcd-community/helm-charts/tree/main/charts"
+  repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2-sync"
   version    = "v2.1.2"
 
@@ -60,7 +60,7 @@ resource "helm_release" "flux2-sync" {
 resource "helm_release" "flux2" {
   name       = "flux2"
   namespace   = var.namespace
-  repository = "https://github.com/fluxcd-community/helm-charts/tree/main/charts"
+  repository = "https://fluxcd-community.github.io/helm-charts"
   chart      = "flux2"
   version    = "v2.1.2"
 
